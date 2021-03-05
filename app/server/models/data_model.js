c76af209f62115efa02ffdd75242a25307518a0b
Adeoluwa <adeoluayobami13@gmail.com>
2021-03-05 16:49:02 +0100

    Added initial JavaScript files to the Project Explorer Project

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -39,13 +39,16 @@ class DataModel {
             return true;
         }
         delete(id){
-            let elementsIndex = this.data.find(element => element.id == id )
-            if(elementsIndex > -1){
-                this.data.splice(elementsIndex, 1);
-                return true;
-            }
-                return false;
-            
+            let user = this.data.find(item => item.id === id);
+
+        let index = this.data.indexOf(user);
+        if (user) {
+            this.data.splice(index, 1);
+            return true;
+        }
+        return false;
+
+    
         }
     // this method will be overriden in the sub classes
     validate(obj) {
